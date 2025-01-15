30948d572503c615e63fd004eccc620984238b47
Will Kiiskila <will.kiiskila@gov.bc.ca>
2025-01-14 22:34:04 -0800

    use AxiosError instead of any

--- src/pages/api/messages.ts
+++ src/pages/api/messages.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -31,11 +31,12 @@ const fetchSpecificMessage = async (id: string) => {
       },
     });
     return data;
-  } catch (error: any) {
-    if (error.response && error.response.status === 404) {
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.response?.status === 404) {
       throw { status: 404, message: "Message not found" };
     }
-    throw error;
+    throw axiosError;
   }
 };
 
@@ -53,9 +54,10 @@ export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const data = await fetchAllMessages();
       res.status(200).json(data);
     }
-  } catch (error: any) {
-    console.error("Error fetching data:", error);
-    if (error.status === 404) {
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    console.error("Error fetching data:", axiosError.message);
+    if (axiosError.response?.status === 404) {
       res.status(404).json({ error: "Message not found" });
     } else {
       res.status(500).json({ error: "Error fetching data" });
